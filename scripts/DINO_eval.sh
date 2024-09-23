411b4999cef26217ec4d0458cd225b5bbbd7b042
Adit <aditdesai2002@gmail.com>
2024-09-24 03:29:07 +0530

    Modified DINO_eval.sh

--- scripts/DINO_eval.sh
+++ scripts/DINO_eval.sh
@@ -4,6 +4,6 @@ python main.py \
   --output_dir logs/DINO/R50-MS4-%j \
 	-c config/DINO/DINO_4scale.py --coco_path $coco_path  \
 	--eval --resume $checkpoint \
-	--options dn_scalar=100 embed_init_tgt=TRUE \
+	--save_results --save_log --options lr=0.00025 epochs=30 num_classes=2 dn_labelbook_size=2 dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
 	dn_box_noise_scale=1.0
