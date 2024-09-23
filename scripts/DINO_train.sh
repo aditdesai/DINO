f87b4a50d9f0173a2b20381b7c6a75f0b79ff6cc
Adit <aditdesai2002@gmail.com>
2024-09-24 02:31:25 +0530

    Modified DINO_train.sh

--- scripts/DINO_train.sh
+++ scripts/DINO_train.sh
@@ -1,6 +1,6 @@
 coco_path=$1
 python main.py \
 	--output_dir logs/DINO/R50-MS4 -c config/DINO/DINO_4scale.py --coco_path $coco_path \
-	--save_results --save_log --options lr=0.00025 epochs=30 num_classes=2 dn_labelbook_size = 2 dn_scalar=100 embed_init_tgt=TRUE \
+	dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
 	dn_box_noise_scale=1.0
