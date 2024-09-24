5f921e7b36ba892ee3382731eba43fe42d3f4b2e
Adit <aditdesai2002@gmail.com>
2024-09-24 11:36:30 +0530

    Modified scripts

--- scripts/DINO_eval_ft.sh
+++ scripts/DINO_eval_ft.sh
@@ -4,6 +4,6 @@ python main.py \
   --output_dir logs/DINO/R50-MS4-%j \
 	-c config/DINO/DINO_4scale_ft.py --coco_path $coco_path  \
 	--eval --resume $checkpoint \
-	--save_results --save_log --options dn_scalar=100 embed_init_tgt=TRUE \
+	--options dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
 	dn_box_noise_scale=1.0
