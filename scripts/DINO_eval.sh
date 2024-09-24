c5aaa407f4e254e747dffe9398dbf1fafda57270
Adit <aditdesai2002@gmail.com>
2024-09-24 10:01:43 +0530

    Modified fine tune params

--- scripts/DINO_eval.sh
+++ scripts/DINO_eval.sh
@@ -2,8 +2,8 @@ coco_path=$1
 checkpoint=$2
 python main.py \
   --output_dir logs/DINO/R50-MS4-%j \
-	-c config/DINO/DINO_4scale.py --coco_path $coco_path  \
+	-c config/DINO/DINO_4scale_ft.py --coco_path $coco_path  \
 	--eval --resume $checkpoint \
-	--save_results --save_log --options lr=0.00025 epochs=30 num_classes=2 dn_labelbook_size=2 dn_scalar=100 embed_init_tgt=TRUE \
+	--save_results --save_log --options dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
 	dn_box_noise_scale=1.0
